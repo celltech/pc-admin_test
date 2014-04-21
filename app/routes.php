b129d1b32d331ddce5ed330b2845c6df0d7153ec
celltech <celltech@users.noreply.github.com>
2014-04-21 16:25:37 -0400

    Test

--- app/routes.php
+++ app/routes.php
@@ -11,12 +11,12 @@
 |
 */
 
-/*Route::get('/', function()
+Route::get('/', function()
 {
 	return View::make('hello');
-});*/
+});
 
-Route::get('/', function()
+Route::get('/test', function()
 {
     return getenv('DB_PASSWORD');
 });
